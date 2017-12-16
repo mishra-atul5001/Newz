62c9f7d3b1a2db53d87dd28ac9c6895b7e45902f
Atul Mishra <atul.mishra5001@gmail.com>
2017-12-16 13:21:46 +0530

    Created News App
    Created news app using API calling via retrofit with good and responsive UI.

---
+++ app/src/main/java/com/example/mishr/newz/RatingActivity.java
@@ -0,0 +1,44 @@
+package com.example.mishr.newz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class RatingActivity extends AppCompatActivity {
+    RatingBar ratingBar;
+    Button rate,back_to_feed;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+        ratingBar = findViewById(R.id.rating_the_app);
+        rate = findViewById(R.id.rate_button);
+        back_to_feed = findViewById(R.id.back_to_feeds);
+        rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String rating = String.valueOf(ratingBar.getRating());
+                Toast.makeText(RatingActivity.this,"You Rated us : "+rating.toString(), Toast.LENGTH_SHORT).show();   // Rating Toast
+
+            }
+        });
+
+        back_to_feed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+}
